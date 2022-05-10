e80345792f6a3d1c7c65eedc8d78c0c52789242e
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-05-10 20:50:47 +0200

    feat: added form validation to tech adv page

--- pages/tech-advisory/index.js
+++ pages/tech-advisory/index.js
@@ -4,22 +4,29 @@ const techAdvFormBtn = document.getElementById("techAdvFormSubmit");
 techAdvFormBtn.addEventListener("click", (event) => {
     event.preventDefault();
 
-    /* Populating the form */
-    const techAdvForm = new ContactRequest(
-        document.getElementById("techAdvFormName").value,
-        document.getElementById("techAdvFormPhone").value,
-        document.getElementById("techAdvFormEmail").value,
-        document.getElementById("techAdvFormMessage").value,
-        document.getElementById("techAdvUserJob").value,
-        document.getElementById("techAdvUserNeededServ").value,
-        document.getElementById("techAdvUserContactMethod").value,
-        document.getElementById("techAdvFormAgreement").checked
-    );
+    /* Check the validity of inputs */
+    const isFormValid = document.getElementById("techAdvForm").checkValidity();
+    if (!isFormValid) {
+        /* If form is not valid, use the usual way to report it to the user */
+        document.getElementById("techAdvForm").reportValidity();
+    } else {
+        /* Populating the form */
+        const techAdvForm = new ContactRequest(
+            document.getElementById("techAdvFormName").value,
+            document.getElementById("techAdvFormPhone").value,
+            document.getElementById("techAdvFormEmail").value,
+            document.getElementById("techAdvFormMessage").value,
+            document.getElementById("techAdvUserJob").value,
+            document.getElementById("techAdvUserNeededServ").value,
+            document.getElementById("techAdvUserContactMethod").value,
+            document.getElementById("techAdvFormAgreement").checked
+        );
 
-    /* Logging */
-    console.table(techAdvForm);
-    console.log(techAdvForm.toJson());
+        /* Logging */
+        console.table(techAdvForm);
+        console.log(techAdvForm.toJson());
 
-    /* Sending form to server */
-    techAdvForm.sendToServer();
+        /* Sending form to server */
+        techAdvForm.sendToServer();
+    }
 });
